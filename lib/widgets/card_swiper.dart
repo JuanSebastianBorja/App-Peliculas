6a99867c37444c8a13f4befd78364b41c10608f0
JuanSebastianBorja <borja.juan@correounivalle.edu.co>
2026-04-22 19:45:10 -0500

    peliculas app

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -20,11 +20,8 @@ class CardSwiper extends StatelessWidget {
         itemBuilder: (_, int index) {
           final movie = movies[index];
           return GestureDetector(
-            onTap: () => Navigator.pushNamed(
-              context,
-              'detail',
-              arguments: 'movie-instance',
-            ),
+            onTap: () =>
+                Navigator.pushNamed(context, 'detail', arguments: movie),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20),
               child: FadeInImage(
